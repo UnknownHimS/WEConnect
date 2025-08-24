3a1c87e795a663291453e8b26fee4759ab0900af
UnknownHimS <resorxvelarde@gmail.com>
2025-08-24 20:23:41 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // PostgreSQL setup
 const pool = new Pool({
